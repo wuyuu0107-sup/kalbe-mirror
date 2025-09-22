5382a0781d6afdb3a81045de1b8f33fedda8b056
bryantwarrickcai <bryantwarrickcai@gmail.com>
2025-09-22 21:06:10 +0700

    red: Modified tests

--- annotation/urls.py
+++ annotation/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('api/v1/documents/<int:document_id>/patients/', views.get_patients_for_document, name='get_patients_for_document'),
     path('api/v1/documents/<int:document_id>/patients/<int:patient_id>/annotations/', views.create_drawing_annotation, name='create_drawing_annotation'),
-    path('api/v1/documents/<int:document_id>/patients/<int:patient_id>/annotations/<int:annotation_id>/', views.drawing_annotation, name='update_drawing_annotation'),
+    path('api/v1/documents/<int:document_id>/patients/<int:patient_id>/annotations/<int:annotation_id>/', views.drawing_annotation, name='drawing_annotation'),
 ]

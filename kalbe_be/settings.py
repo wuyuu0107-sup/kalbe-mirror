8e8d5a355f42c6ab82e0463503d644533e98e417
bryantwarrickcai <bryantwarrickcai@gmail.com>
2025-09-27 17:31:16 +0700

    xiao

--- kalbe_be/settings.py
+++ kalbe_be/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +77,9 @@ WSGI_APPLICATION = 'kalbe_be.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config("DATABASE_URL")
+    )
 }
 
 

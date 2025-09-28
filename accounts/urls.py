7a1b26fc3a6499221477da313fd0859df2903679
Geordie Vannese <geordievannese01@gmail.com>
2025-09-28 09:41:17 +0700

    [GREEN] Add URL routes for email OTP password reset request, confirm, and test page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from . import views
+from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+from . import views_otp_email as v_email
+
+@method_decorator(ensure_csrf_cookie, name="dispatch")
+class EmailOTPTestPage(TemplateView):
+    template_name = "accounts/otp_email_test.html"
 
 urlpatterns = [
-    path("password-reset/request/", views.password_reset_request, name="password-reset-request"),
-    path("password-reset/confirm/<uidb64>/<token>/", views.password_reset_confirm, name="password-reset-confirm"),
+    # Email OTP
+    path("password-reset/otp/request/", v_email.password_reset_otp_request, name="password-reset-otp-request"),
+    path("password-reset/otp/confirm/", v_email.password_reset_otp_confirm, name="password-reset-otp-confirm"),
+    path("password-reset/otp/test/", EmailOTPTestPage.as_view(), name="password-reset-otp-test"),
 ]

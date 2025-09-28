73d3916e1453338bb1392905579a3109fd3b5415
KeiraDiaz <nkeiradiaz@gmail.com>
2025-09-28 16:43:32 +0700

    chore: changed settings and .env file

--- kalbe_be/settings.py
+++ kalbe_be/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-461+y(vuzlus9-rs6rwp_r%w-9$3h)6tsk^qizulq*h3w$7j-0'
+SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

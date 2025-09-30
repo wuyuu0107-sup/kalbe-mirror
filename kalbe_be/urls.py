896ec512c6f9609b54bc5c5446695cc79efe7037
Hafizh Zen <hafizh.suryamustafa@gmail.com>
2025-09-30 21:34:43 +0700

    [REFACTOR] fixed urls.py

--- kalbe_be/urls.py
+++ kalbe_be/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('api/protected-endpoint/', protected_endpoint),
     path('', include('annotation.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

17e659b0d21719202587460e8ad2afafc3f7de74
sugonamu <je0jwff@gmail.com>
2025-09-30 14:30:34 +0700

    [REFACTOR]: Removed EasyOCR and Tests on EasyOCR

--- ocr/urls.py
+++ ocr/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ocr_test_page, health, api_ocr
+from .views import ocr_test_page, health
 
 app_name = "ocr"
 
 urlpatterns = [
     path("health/", health, name="health"),
     path("", ocr_test_page, name="ocr_test_page"),
-    path("api/ocr/", api_ocr, name="api_ocr"),
 ]

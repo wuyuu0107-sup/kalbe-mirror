5bf4929bc8f000b8515c2e2848e0ad7e3dd28b96
Geordie Vannese <geordievannese01@gmail.com>
2025-09-30 16:45:33 +0700

    Fixing Issue

--- kalbe_be/settings.py
+++ kalbe_be/settings.py
@@ -51,6 +51,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+]
 
 ALLOWED_HOSTS = [
     'backend-mirror-production.up.railway.app',

df754b33f78738aa52b8fff3b7f4882c23288296
KeiraDiaz <nkeiradiaz@gmail.com>
2025-10-09 03:49:41 +0700

    [GREEN] implemented model for CSV

--- save_to_database/models.py
+++ save_to_database/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class CSV(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='datasets/csvs/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    source_json = models.JSONField(blank=True, null=True)
+    record_count = models.IntegerField(default=0)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name

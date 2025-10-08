9bd7a50e66aebf84d12a27412ad63f116fef6067
wuyu0107 <mkim050107@gmail.com>
2025-10-08 22:03:45 +0700

    [GREEN] Implement get_recent_files service to pass test

--- dashboard/services/recent_files.py
+++ dashboard/services/recent_files.py
@@ -2,4 +2,15 @@ from datetime import datetime, MINYEAR
 from ..storage import get_storage
 
 def get_recent_files(limit=10):
-    return
+    storage = get_storage()
+    items = list(storage.list_csv(limit=limit*3))
+
+    norm = []
+    for i in items:
+        u = i.get("updated_at")
+        if isinstance(u, str):
+            u = datetime.fromisoformat(u.replace("Z", "+00:00")).replace(tzinfo=None)
+        norm.append({**i, "updated_at": u})
+
+    norm.sort(key=lambda x: x["updated_at"] or datetime(MINYEAR, 1, 1), reverse=True)
+    return norm[:limit]

fa8243805b3bc1b1634c1a9de6be0e1f49f1b873
KeiraDiaz <nkeiradiaz@gmail.com>
2025-10-12 20:11:34 +0700

    [GREEN] implemented observer design pattern using signals and also app config

--- save_to_database/apps.py
+++ save_to_database/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class SaveToDatabaseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'save_to_database'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "save_to_database"
+
+    def ready(self):
+        from . import signals  # noqa: F401

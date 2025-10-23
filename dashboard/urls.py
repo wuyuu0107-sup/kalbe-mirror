a9eadfe272db32ad3767e11d1bd0cdefb9787237
wuyu0107 <mkim050107@gmail.com>
2025-10-23 21:07:54 +0700

    chore: implement a function to send user info

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dashboard.views import recent_files_json, recent_features_json
+from dashboard.views import recent_files_json, recent_features_json, whoami
 from . import views
 
 app_name = "dashboard"
@@ -8,5 +8,5 @@ urlpatterns = [
     path("recent-features/", recent_features_json, name="recent-features-json"),
     path("breadcrumbs/", views.breadcrumbs_json, name="breadcrumbs"),
     path("breadcrumbs_demo/", views.breadcrumbs_demo, name="breadcrumbs_demo"),
-
+    path("user", whoami, name="whoami")
 ]

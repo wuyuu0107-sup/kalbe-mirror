11b4b37e7a953a9df63b892b4249eb2361573d11
KeiraDiaz <nkeiradiaz@gmail.com>
2025-10-29 23:30:13 +0700

    [RED] implemented views.py in search app

--- search/views.py
+++ search/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .services import search_storage_files
 
-# Create your views here.
+def search_files(request):
+    bucket = request.GET.get('bucket')
+    term = request.GET.get('q')
+    ext = request.GET.get('ext')
+    
+    if not bucket or not term:
+        return JsonResponse({
+            'error': 'Missing required parameters'
+        }, status=400)
+    
+    try:
+        results = search_storage_files(bucket, term, ext)
+        return JsonResponse({'files': results})
+    except Exception as e:
+        return JsonResponse({
+            'error': str(e)
+        }, status=500)

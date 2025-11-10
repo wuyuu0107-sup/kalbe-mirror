4a295fd1d7a16635c027406e1e89086bc227c0e2
bryantwarrickcai <bryantwarrickcai@gmail.com>
2025-11-10 20:50:31 +0700

    [GREEN] Added code for folder deletion

--- dataset/urls.py
+++ dataset/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CSVFileListCreateView, CSVFileRetrieveDestroyView, CSVFileDownloadView, CSVFileMoveView, FolderMoveView
+from .views import CSVFileListCreateView, CSVFileRetrieveDestroyView, CSVFileDownloadView, CSVFileMoveView, FolderMoveView, FolderDeleteView
 
 urlpatterns = [
     path("files/", CSVFileListCreateView.as_view(), name="csvfile_list_create"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("files/<int:pk>/download/", CSVFileDownloadView.as_view(), name="csvfile_download"),
     path("files/<int:pk>/move/", CSVFileMoveView.as_view(), name="csvfile_move"),
     path("folders/move/", FolderMoveView.as_view(), name="folder_move"),
+    path("folders/delete/", FolderDeleteView.as_view(), name="folder_delete"),
 ]

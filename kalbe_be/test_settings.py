43e85495aec856a4a71ac538300d8fb034236572
sugonamu <je0jwff@gmail.com>
2025-11-27 00:13:09 +0700

    fix: failing CI issue

--- kalbe_be/test_settings.py
+++ kalbe_be/test_settings.py
@@ -1,12 +1,14 @@
-# To run all tests:
-# coverage run --source=. manage.py test --settings=kalbe_be.test_settings
+from . import settings as base
 
+# copy all UPPERCASE settings from base into this module
+for name in dir(base):
+    if name.isupper():
+        globals()[name] = getattr(base, name)
+
+# override DB for tests
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",  
+        "NAME": ":memory:",
     }
 }
-
-print(">>> USING TEST_SETTINGS (SQLite) <<<")
-print(DATABASES)
